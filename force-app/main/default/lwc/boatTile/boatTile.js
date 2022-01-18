436fe96ef1e932f1c887e3f9851fb989aaedc7f3
Gokul Bhavesh Joshi <Gokulbhavesh2312@gmail.com>
2022-01-18 18:01:38 +0530

    all challenges completed

---
+++ force-app/main/default/lwc/boatTile/boatTile.js
@@ -0,0 +1,26 @@
+import { api, LightningElement } from 'lwc';
+const TILE_WRAPPER_SELECTED_CLASS = "tile-wrapper selected";
+const TILE_WRAPPER_UNSELECTED_CLASS = "tile-wrapper";
+
+export default class BoatTile extends LightningElement {
+    @api boat;
+    @api selectedBoatId;
+    get backgroundStyle() {
+        console.log('image', this.boat.Picture__c);
+        return  `background-image:url(${this.boat.Picture__c})`;
+    }
+    
+    get tileClass() {
+        return this.selectedBoatId ? TILE_WRAPPER_SELECTED_CLASS : TILE_WRAPPER_UNSELECTED_CLASS;
+    }
+     selectBoat() {
+        this.selectedBoatId = !this.selectedBoatId;
+        const boatselect = new CustomEvent("boatselect", {
+        detail: {
+        boatId: this.boat.Id
+        }
+        });
+        this.dispatchEvent(boatselect);
+        }
+    
+}
